f6c659f74bd483d02d41f548d0f2406389a24413
Sergio Marchio <sergiomarchio@gmail.com>
2022-11-08 10:52:58 -0300

    added cors settings

--- api/main.py
+++ api/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from topic import topic_lda
 
+
 app = FastAPI()
+
+origins = [
+    "https://question-ai.api.serg.ink",
+    "https://question-ai.serg.ink",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    # allow_credentials=True,
+    # allow_methods=["*"],
+    # allow_headers=["*"],
+)
 
 
 @app.get("/topic")
