e243f6e84db4e122ede53760e6cfc0baa4084e32
Sergio Marchio <sergiomarchio@gmail.com>
2022-11-10 23:30:45 -0300

    added /topics endpoint

--- web/main.js
+++ web/main.js
@@ -1,7 +1,7 @@
 function go() {
     console.log(inputQuestion.value);
 
-    get_topic(inputQuestion.value, (v) => {
+    getTopic(inputQuestion.value, (v) => {
         topicResponse.textContent = JSON.stringify(v, null, 2);
         topicName.textContent = v["topic-name"];
     });
@@ -10,7 +10,7 @@ function go() {
     topicName.value = "";
 }
 
-function get_topic(question, action) {
+function getTopic(question, action) {
     fetch(topicURL + new URLSearchParams({question: question}), {
         method: "GET",
     })
@@ -20,20 +20,49 @@ function get_topic(question, action) {
 
 }
 
+function getTopics(action) {
+    fetch(topicsURL, {
+        method: "GET",
+    })
+        .then(r => r.json()
+            .then(j => action(j)))
+        .catch(e => console.log(e));
+
+}
+
+function populateTopics() {
+    topicsLinkChange();
+    getTopics(v => {
+        topicsResponse.textContent = JSON.stringify(v, null, 2);
+        topicsNames.textContent = Object.values(v).join(", ");
+    });
+}
+
 function topicLinkChange() {
     let linkValue = topicURL + "question=" + inputQuestion.value;
     topicLink.textContent = linkValue;
     topicLink.setAttribute("href", linkValue);
 }
 
+function topicsLinkChange() {
+    let linkValue = topicsURL;
+    topicsLink.textContent = linkValue;
+    topicsLink.setAttribute("href", linkValue);
+}
 
-const topicURL = "https://question-ai.api.serg.ink/topic?"
+
+const baseURL = "https://question-ai.api.serg.ink"
+const topicURL = baseURL + "/topic?"
+const topicsURL = baseURL + "/topics"
 
 const inputQuestion = document.querySelector("input#question");
 const buttonGo = document.querySelector("button#go");
 const topicName = document.querySelector("#topic-name");
+const topicsNames = document.querySelector("#topics-names");
 const topicLink = document.querySelector("#topic-link");
+const topicsLink = document.querySelector("#topics-link");
 const topicResponse = document.querySelector("#topic-response");
+const topicsResponse = document.querySelector("#topics-response");
 
 
 buttonGo.addEventListener("click", go);
@@ -47,6 +76,7 @@ inputQuestion.addEventListener("keyup", (e) => {
 
 
 window.onload = function () {
+    populateTopics();
     inputQuestion.focus();
     topicLinkChange();
 }
